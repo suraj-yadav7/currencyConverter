5d6ac24eda5cf3cbea7886cce6ea79466e203d6e
suraj-yadav7 <112300059+suraj-yadav7@users.noreply.github.com>
2023-10-12 08:05:05 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import CurrencyMain from "/src/components/04CurrencyConv/CurrencyMain.jsx"
+
+export default function App() {
+  return (
+    <>
+    <CurrencyMain/>
+    </>
+  )
+}
---
+++ src/components/04CurrencyConv/Currencyconverter.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const Currencyconverter = () => {
+  return (
+    <>
+      <div>
+        <p className="text-green bg-green-200">This is currency converter</p>
+      </div>
+    </>
+  )
+}
+
+export default Currencyconverter;
